06222085251e81860638a8d841706e458073b8d6
Hilaria Romero <hilariarb@gmail.com>
2025-12-22 20:58:25 +0100

    integracion firebase pt1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
+    id("com.android.library") apply false
 }
 
 android {
@@ -45,8 +47,16 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
-    implementation("org.mongodb:mongodb-driver:3.12.14")
-    implementation("io.github.cdimascio:dotenv-java:3.0.0")
+    implementation("com.google.gms:google-services:4.4.4") //4.3.15->4.4.4 (catalog)
+    implementation("com.google.android.gms:auth-api-impl:11.6.0") // catalog
+    implementation("com.google.firebase:firebase-config-ktx:22.1.2") //19.2.0->22.1.2 (catalog)
+    implementation(platform("com.google.firebase:firebase-bom:34.7.0")) //catalog
+    implementation("com.google.firebase:firebase-firestore:26.0.2") //catalog
+
+    implementation("androidx.appcompat:appcompat:1.7.1") // existing catalog libs.appcompat
+    implementation("com.google.android.material:material:1.13.0") //1.9.0->1.13.0 (already as 'material')
+    implementation("com.google.firebase:firebase-auth:24.0.1") //catalog
+    implementation("com.google.firebase:firebase-messaging:25.0.1") //catalog
 }
 
 
